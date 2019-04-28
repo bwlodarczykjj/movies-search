2087251a0970e37f8a8fe1355d0ae8edef9bbe8f
bwlodarczykjj <b.wlodarczykjj@gmail.com>
2019-04-28 16:53:24 +0200

    dupa

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          zmiana
+          <p>asdasdasdasd</p>
         </a>
       </header>
     </div>
